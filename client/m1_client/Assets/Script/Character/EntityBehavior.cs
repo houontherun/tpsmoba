5673156bb97a44365c26c0c504333be6193271a7
yanwei2016 <yanwei47@163.com>
2017-07-06 17:29:16 +0800

    添加实体类 entity

---
+++ client/m1_client/Assets/Script/Character/EntityBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityBehavior : MonoBehaviour {
+
+    private Entity pEntity;
+	
+    public void SetEntityProperty(Entity entity)
+    {
+        pEntity = entity;
+    }
+
+    public Entity GetEntity()
+    {
+        return pEntity;
+    }
+}
